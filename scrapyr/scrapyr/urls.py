be4869b914d42d5b30309d92d7af718ae016233b
James Carl <mrjamescarl@gmail.com>
2015-02-27 21:17:48 -0500

    Full stocks listing page setup

--- scrapyr/scrapyr/urls.py
+++ scrapyr/scrapyr/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     url(r'^$', include('scrapyr_app.urls')),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^login/$', 'scrapyr_app.views.login'),
-    url(r'^home/$', 'scrapyr_app.views.home'),
+    url(r'^home/$', 'scrapyr_app.views.dashboard'),
     url(r'^logout/$', 'scrapyr_app.views.logout'),
     url(r'^profile/$', 'scrapyr_app.views.profile'),
+    url(r'^stocks/$', 'scrapyr_app.views.stocks'),
+    url(r'^stock/$', 'scrapyr_app.views.stock'),
 ]
