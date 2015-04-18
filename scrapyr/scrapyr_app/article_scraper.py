74a330a2fcef712bdbf3f8dd1ecf3a8ec495bc16
Michelle <mrojass1028@gmail.com>
2015-04-18 13:58:23 -0400

    scraper updated

--- scrapyr/scrapyr_app/article_scraper.py
+++ scrapyr/scrapyr_app/article_scraper.py
@@ -1,9 +1,16 @@
+import requests
+import re
 from newspaper import Article
 from lxml import html
-import requests
 from bs4 import BeautifulSoup
 
 article_websites = ["www.techcrunch.com"]
+article_url_re = re.compile('http://techcrunch\.com/20[0-9][0-9]/[0-1][0-9]/[0-3][0-9]/.*')
+#article_comment_re = re.compile('http://techcrunch\.com/20[0-9][0-9]/[0-1][0-9]/[0-3][0-9]/.*/#comments')
+
+articles = []
+
+
 
 for site in article_websites:
 	r = requests.get("http://" + site)
@@ -11,5 +18,28 @@ for site in article_websites:
 
 	soup = BeautifulSoup(data)
 	for link in soup.find_all('a'):
-		print link.get('href')
+		article_link = link.get('href')
+		try:
+			if article_url_re.match(article_link):
+				if article_link not in articles and not article_link.endswith('#comments'):
+					articles.append(article_link)
+					# print article_link
+		except AttributeError as ae:
+			print str(ae)
+		except TypeError as te:
+			print str(te)
+
+
+for article in articles:
+	a = Article(article)
+	a.download()
+	a.parse()
+	# print a.title
+	a.nlp()
+	print a.keywords
+
+
+
+
+
 		
